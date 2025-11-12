6fe22e64984305615724f546daeca10e39f64943
Otto Freitag <ottofreitag@uol.com.br>
2025-11-12 08:39:58 -0300

    💱 Expandir lista de moedas suportadas para 90+ moedas
    📍 Regiões expandidas:
    - América do Sul: +3 moedas (VES, GYD, SRD)
    - América Central e Caribe: +11 moedas (CRC, GTQ, HNL, NIO, PAB, DOP, HTG, JMD, TTD, BBD)
    - Europa: +13 moedas (NOK, SEK, DKK, ISK, PLN, CZK, HUF, RON, BGN, HRK, RUB, UAH, TRY)
    - Ásia: +11 moedas (HKD, TWD, SGD, MYR, THB, IDR, PHP, VND, PKR, BDT, LKR, NPR)
    - Oriente Médio: +8 moedas (AED, SAR, QAR, KWD, BHD, OMR, JOD, ILS)
    - África: +9 moedas (ZAR, EGP, NGN, KES, GHS, TZS, UGX, MAD, TND)
    - Oceania: +2 moedas (FJD, PGK)
    - Metais Preciosos: +2 (XAU ouro, XAG prata)
    
    Total: 90+ moedas de todos os continentes! 🌍

--- src/main/java/com/otto/conversormoedas/model/CurrencyCode.java
+++ src/main/java/com/otto/conversormoedas/model/CurrencyCode.java
@@ -11,26 +11,93 @@ public enum CurrencyCode {
     PEN("Sol peruano"),
     UYU("Peso uruguaio"),
     PYG("Guarani paraguaio"),
+    VES("Bolívar venezuelano"),
+    GYD("Dólar guianense"),
+    SRD("Dólar surinamês"),
     
     // Moedas da América do Norte
     USD("Dólar americano"),
     CAD("Dólar canadense"),
     MXN("Peso mexicano"),
     
+    // Moedas da América Central e Caribe
+    CRC("Colón costarriquenho"),
+    GTQ("Quetzal guatemalteco"),
+    HNL("Lempira hondurenha"),
+    NIO("Córdoba nicaraguense"),
+    PAB("Balboa panamenho"),
+    DOP("Peso dominicano"),
+    HTG("Gourde haitiano"),
+    JMD("Dólar jamaicano"),
+    TTD("Dólar trinitário"),
+    BBD("Dólar barbadiano"),
+    
     // Moedas Europeias
     EUR("Euro"),
     GBP("Libra esterlina"),
     CHF("Franco suíço"),
+    NOK("Coroa norueguesa"),
+    SEK("Coroa sueca"),
+    DKK("Coroa dinamarquesa"),
+    ISK("Coroa islandesa"),
+    PLN("Zloty polonês"),
+    CZK("Coroa tcheca"),
+    HUF("Forint húngaro"),
+    RON("Leu romeno"),
+    BGN("Lev búlgaro"),
+    HRK("Kuna croata"),
+    RUB("Rublo russo"),
+    UAH("Hryvnia ucraniana"),
+    TRY("Lira turca"),
     
     // Moedas Asiáticas
     JPY("Iene japonês"),
     CNY("Yuan chinês"),
-    INR("Rúpia indiana"),
+    HKD("Dólar de Hong Kong"),
+    TWD("Dólar taiwanês"),
     KRW("Won sul-coreano"),
+    SGD("Dólar singapuriano"),
+    MYR("Ringgit malaio"),
+    THB("Baht tailandês"),
+    IDR("Rupia indonésia"),
+    PHP("Peso filipino"),
+    VND("Dong vietnamita"),
+    INR("Rúpia indiana"),
+    PKR("Rúpia paquistanesa"),
+    BDT("Taka bengali"),
+    LKR("Rúpia do Sri Lanka"),
+    NPR("Rúpia nepalesa"),
+    
+    // Moedas do Oriente Médio
+    AED("Dirham dos Emirados"),
+    SAR("Riyal saudita"),
+    QAR("Riyal catariano"),
+    KWD("Dinar kuwaitiano"),
+    BHD("Dinar bareinita"),
+    OMR("Rial omanense"),
+    JOD("Dinar jordaniano"),
+    ILS("Novo shekel israelense"),
+    
+    // Moedas Africanas
+    ZAR("Rand sul-africano"),
+    EGP("Libra egípcia"),
+    NGN("Naira nigeriana"),
+    KES("Xelim queniano"),
+    GHS("Cedi ganês"),
+    TZS("Xelim tanzaniano"),
+    UGX("Xelim ugandense"),
+    MAD("Dirham marroquino"),
+    TND("Dinar tunisiano"),
     
     // Moedas da Oceania
     AUD("Dólar australiano"),
-    NZD("Dólar neozelandês");
+    NZD("Dólar neozelandês"),
+    FJD("Dólar fijiano"),
+    PGK("Kina papuásia"),
+    
+    // Outras Moedas Importantes
+    XAU("Ouro (onça troy)"),
+    XAG("Prata (onça troy)");
 
     private final String description;
 
